a951d7d09040cb152985aa4c593eb45f38f598cc
clase <josemariamataojeda@gmail.com>
2021-10-25 14:37:42 +0200

    j

---
+++ U10_Ej1_UWPBinding/U10_Ej1_UWPBinding/Models/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Persona
+    {
+        #region atributos privados
+        private String nombre;
+        private String apellido;
+        #endregion
+        #region constructores
+        public Persona()
+        {
+            this.nombre = "Jose Maria";
+            this.apellido = "Mata Ojeda";
+        }
+        public Persona(String nombre, String apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        #endregion
+        #region propiedades publicas
+        public String Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+        public String Apellido
+        {
+            get => apellido;
+            set => apellido = value;
+        }
+        #endregion
+
+    }
+}
---
+++ U10_Ej1_UWPBinding/U10_Ej1_UWPBinding/ViewModels/Ejercicio2VM.cs
@@ -0,0 +1,16 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U10_Ej1_UWPBinding.ViewModels
+{
+    public class Ejercicio2VM
+    {
+        private Persona person;
+
+        public Persona Person { get => person; set => person = value; }
+    }
+}
