4467c02c3b9b70de6437ab40f392c82b42b56d3b
josmatoje <josemariamataojeda@gmail.com>
2021-12-09 00:13:06 +0100

    Auto stash before merge of "main" and "origin/main"

--- U10_Ej1_UWPBinding/U10_Ej1_UWPBinding/App.xaml.cs
+++ U10_Ej1_UWPBinding/U10_Ej1_UWPBinding/App.xaml.cs
@@ -67,7 +67,7 @@ namespace U10_Ej1_UWPBinding
                     // Cuando no se restaura la pila de navegación, navegar a la primera página,
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
-                    rootFrame.Navigate(typeof(Ejercicio4), e.Arguments);
+                    rootFrame.Navigate(typeof(Ejercicio2), e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
